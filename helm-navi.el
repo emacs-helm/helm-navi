eb5d53f2abc640bea90e3c221562913c4d144638
Adam Porter <adam@alphapapa.net>
2017-04-02 09:51:30 -0500

    Fix description and add links in readme

--- helm-navi.el
+++ helm-navi.el
@@ -31,8 +31,8 @@
 
 ;; Outshine <https://github.com/tj64/outshine> is a package that lets
 ;; you organize your non-org-mode buffers with commented Org headings
-;; and other Org features, especially useful for any kind source code,
-;; or any file format which supports comments.
+;; and other Org features, especially useful for any kind of source
+;; code, or any file format which supports comments.
 
 ;; These packages are practically developed as as single package, and
 ;; `navi-mode' requires `outshine', so they are supported here in a
